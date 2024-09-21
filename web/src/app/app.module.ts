bb950023e4c47d5422b27d24d81a1866a6c35ca3
wangyuhanhhh <chaoxi_0310@qq.com>
2024-09-21 11:40:30 +0800

    12

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TermComponent } from './term/term.component';
+import { SchoolComponent } from './school/school.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TermComponent
+    TermComponent,
+    SchoolComponent
   ],
   imports: [
     BrowserModule,
