28cf9612dd8b65cc6621b35b771fb1f54a13f809
wangyuhanhhh <chaoxi_0310@qq.com>
2024-09-28 19:13:35 +0800

    25

--- htdocs/thinkphp5/application/common/model/School.php
+++ htdocs/thinkphp5/application/common/model/School.php
@@ -3,5 +3,4 @@ namespace app\common\model;
 use think\Model;
 
 class School extends Model {
-
 }
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -6,8 +6,8 @@ import { AppComponent } from './app.component';
 import { TermComponent } from './term/term.component';
 import { SchoolComponent } from './school/school.component';
 import { HttpClientModule } from '@angular/common/http';
-import { AddComponent } from './school/add/add.component';
-import { AddComponent } from './term/add/add.component';
+import { AddComponent as SchoolAddComponent} from './school/add/add.component';
+import { AddComponent as TermAddComponent } from './term/add/add.component';
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
@@ -15,6 +15,7 @@ import en from '@angular/common/locales/en';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import {RouterModule} from '@angular/router';
 
 registerLocaleData(en);
 
@@ -23,7 +24,8 @@ registerLocaleData(en);
     AppComponent,
     TermComponent,
     SchoolComponent,
-    AddComponent
+    SchoolAddComponent,
+    TermAddComponent
   ],
   imports: [
     BrowserModule,
@@ -31,7 +33,8 @@ registerLocaleData(en);
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
-    NzDatePickerModule
+    NzDatePickerModule,
+    RouterModule
   ],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
